216105eabbc5129ee6a14e07ee7bf14c16f050a1
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-04-04 18:30:49 -0300

    initial commit, initial structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import nlwUniteIconHeader from '../assets/header-nlw-icon-header.svg'
+
+export function Header(){
+  return (
+    <div className='flex items-center gap-5 py-2'>
+      <img src={nlwUniteIconHeader} alt="Icone nlw" />
+
+      <nav className='flex items-center gap-5'>
+        <a href="" className='font-medium text-sm text-zinc-300'>Eventos</a>
+        <a href="" className='font-medium text-sm'>Participantes</a>
+      </nav>
+    </div>
+  )
+}
